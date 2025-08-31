087a4fa95ebdfa0a20bddbee2a2374c018d6dac5
Endi-Ostander <aleksandraleksandrovich139@gmail.com>
2025-08-31 19:32:42 +0500

    chore: initial commit with Jest + Babel config

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export function sum(a, b) { return a + b; }
+
+if (process.env.RUN_APP === '1') {
+  console.log('App started');
+}
---
+++ tests/sum.test.js
@@ -0,0 +1,5 @@
+import { sum } from '../src/index.js';
+
+test('sum works', () => {
+  expect(sum(2, 3)).toBe(5);
+});
